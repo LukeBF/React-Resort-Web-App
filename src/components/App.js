7c137d49ac9de525f42cf5a77cd9679e6a5b13f1
Luke Blache-Fraser <resolvetechnologiestt@gmail.com>
2021-06-06 22:12:23 -0400

    React initial folder structure setup.Tested dispaying UI to the browser-successful

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+//This is the root component
+import React from 'react'
+import "../assets/css/App.css"
+
+
+const App = () => {
+  return (
+    <div>
+        <h1>Luke Blache-Fraser</h1>
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
+// This is the entry point file
+
+/**
+ * The purpose of this file is to take the root component, which
+ * is App and and inject that component into the 'root' div.
+ * This will cause the app to be seen
+ */
+
+import React from 'react'; //Not required
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './components/App'; //Root component
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//Call the App component as a tag which was imported above
+ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
