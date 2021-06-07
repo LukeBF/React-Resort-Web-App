b44f3cbb5fa7416c3fe92dc017ddb2a810f3e9e7
Luke Blache-Fraser <resolvetechnologiestt@gmail.com>
2021-06-07 18:30:28 -0400

    Components created with test HTML printing to the Home page

---
+++ notes.md
@@ -0,0 +1,10 @@
+#Components for the Resort app:
+1. Header
+2. Footer
+3. Resort listing
+4. Resort card
+5. Hero
+
+Components are reusable on multiple pages.
+
+App.js is the root that is bringing all the components together
--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,20 @@
 //This is the root component
 import React from 'react'
+import Header from './Header'
+import Hero from './Hero'
+import ResortListing from './ResortListing'
+import Footer from './Footer'
+
 import "../assets/css/App.css"
 
-
+//Root component
 const App = () => {
   return (
     <div>
-        <h1>Luke Blache-Fraser</h1>
+        <Header />
+        <Hero />
+        <ResortListing />
+        <Footer />
     </div>
   );
 }
